a042498fdf882d2e887dee7afc52241f15af5b20
Subeom Kwon <subeomkwon@gmail.com>
2024-02-01 21:04:28 +0900

    일기 생성 기능 구현 및 테스트코드 작성

---
+++ weather/src/main/java/com/example/weather/controller/DiaryController.java
@@ -0,0 +1,26 @@
+package com.example.weather.controller;
+
+import com.example.weather.service.DiaryService;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+
+@RestController
+public class DiaryController {
+    private final DiaryService diaryService;
+
+    public DiaryController(DiaryService diaryService) {
+        this.diaryService = diaryService;
+    }
+
+    @PostMapping("/create/diary")
+    void createDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date, @RequestBody String text) {
+        diaryService.createDiary(date, text);
+    }
+
+}
---
+++ weather/src/test/java/com/example/weather/JpaMemoRepositoryTest.java
@@ -0,0 +1,45 @@
+package com.example.weather;
+
+import com.example.weather.domain.Memo;
+import com.example.weather.repository.JpaMemoRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+public class JpaMemoRepositoryTest {
+
+    @Autowired
+    JpaMemoRepository jpaMemoRepository;
+
+    @Test
+    void insertMemoTest(){
+        //given
+        Memo newMemo = new Memo(10, "this is jpa memo");
+        //when
+        jpaMemoRepository.save(newMemo);
+        //then
+        List<Memo> memoList = jpaMemoRepository.findAll();
+        System.out.println(memoList);
+        assertFalse(memoList.isEmpty());
+    }
+
+    @Test
+    void findByIdTest(){
+        //given
+        Memo newMemo = new Memo(11, "jpa");
+        //when
+        Memo memo = jpaMemoRepository.save(newMemo);
+        System.out.println(memo.getId());
+        //then
+        Optional<Memo> result = jpaMemoRepository.findById(memo.getId());
+        assertEquals(result.get().getText(), "jpa");
+    }
+}
