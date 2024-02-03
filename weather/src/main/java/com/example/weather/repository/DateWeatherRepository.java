2bc10370bedf32f726553030b51a371886a82381
Subeom Kwon <subeomkwon@gmail.com>
2024-02-04 00:04:54 +0900

    매일 새벽 1시에 어제 날씨데이터를 DB로 가져오도록하는 스케줄링 로직 구현 + Log기능 추가

---
+++ weather/src/main/java/com/example/weather/repository/DateWeatherRepository.java
@@ -0,0 +1,14 @@
+package com.example.weather.repository;
+
+import com.example.weather.domain.DateWeather;
+import org.springframework.cglib.core.Local;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface DateWeatherRepository extends JpaRepository<DateWeather, LocalDate> {
+    List<DateWeather> findAllByDate(LocalDate localDate);
+}
--- weather/src/main/java/com/example/weather/service/DiaryService.java
+++ weather/src/main/java/com/example/weather/service/DiaryService.java
@@ -1,13 +1,18 @@
 package com.example.weather.service;
 
+import com.example.weather.WeatherApplication;
+import com.example.weather.domain.DateWeather;
 import com.example.weather.domain.Diary;
+import com.example.weather.repository.DateWeatherRepository;
 import com.example.weather.repository.DiaryRepository;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cglib.core.Local;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,34 +32,63 @@ import java.util.Map;
 public class DiaryService {
     @Value("${openweathermap.key}")
     private String apiKey;
-
+    private static final Logger logger = LoggerFactory.getLogger(WeatherApplication.class);
     private final DiaryRepository diaryRepository;
+    private final DateWeatherRepository dateWeatherRepository;
 
-    public DiaryService(DiaryRepository diaryRepository) {
+    public DiaryService(DiaryRepository diaryRepository, DateWeatherRepository dateWeatherRepository) {
         this.diaryRepository = diaryRepository;
+        this.dateWeatherRepository = dateWeatherRepository;
     }
 
-
+    @Transactional
+    @Scheduled(cron = "0 0 1 * * * ")
+//    @Scheduled(cron = "0/5 * * * * * ") test용
+    public void saveWeatherDate() throws IOException {
+        dateWeatherRepository.save(getWeatherFromApi());
+    }
     @Transactional(isolation = Isolation.SERIALIZABLE)
     public void createDiary(LocalDate date, String text) throws IOException {
+        logger.info("started to create diary");
+        //날씨 데이터 가져오기 (DB에서 캐시된 날씨 데이터 가져오기)
+        DateWeather dateWeather = getDateWeather(date);
+        //파싱된 데이터 + 일기 값 DB에 저장
+        Diary nowDiary = new Diary();
+        nowDiary.setDateWeather(dateWeather);
+        nowDiary.setText(text);
+        diaryRepository.save(nowDiary);
+        logger.info("end to create diary");
+    }
+
+    private DateWeather getWeatherFromApi() throws IOException {
         //open weather map에서 날씨 데이터 가져오기
         String weatherData = getWeatherString();
 
         //받아온 날씨 json 파싱
         Map<String, Object> parsedWeather = parseWeather(weatherData);
-        //파싱된 데이터 + 일기 값 DB에 저장
-        Diary nowDiary = new Diary();
-        nowDiary.setWeather(parsedWeather.get("main").toString());
-        nowDiary.setIcon(parsedWeather.get("icon").toString());
-        nowDiary.setTemperature((Double) parsedWeather.get("temp"));
-        nowDiary.setText(text);
-        nowDiary.setDate(date);
-        diaryRepository.save(nowDiary);
-
+        DateWeather dateWeather = new DateWeather();
+        dateWeather.setDate(LocalDate.now());
+        dateWeather.setWeather(parsedWeather.get("main").toString());
+        dateWeather.setIcon(parsedWeather.get("icon").toString());
+        dateWeather.setTemperature((Double) parsedWeather.get("temp"));
+        return dateWeather;
     }
+
+    private  DateWeather getDateWeather(LocalDate date) throws IOException {
+        List<DateWeather> dateWeatherListFromDB = dateWeatherRepository.findAllByDate(date);
+        if(dateWeatherListFromDB.isEmpty()){
+            //새로 api에서 날씨정보를 가져와야함
+            return getWeatherFromApi();
+        }
+        else{
+            return dateWeatherListFromDB.get(0);
+        }
+    }
+
 
     @Transactional(readOnly = true)
     public List<Diary> readDiary(LocalDate date){
+        logger.debug("started to read diary");
         return diaryRepository.findAllByDate(date);
     }
 
