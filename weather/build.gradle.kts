4509b834d0e427305b52d449d7a9564e1e7153b1
Subeom Kwon <subeomkwon@gmail.com>
2024-02-05 15:21:22 +0900

    API documentation을 위한 swagger추가

--- weather/build.gradle.kts
+++ weather/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
 	java
-	id("org.springframework.boot") version "3.2.2"
+	id("org.springframework.boot") version "2.7.18"
 	id("io.spring.dependency-management") version "1.1.4"
 }
 
@@ -26,6 +26,10 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-jdbc")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("com.googlecode.json-simple:json-simple:1.1.1")
+	implementation("io.springfox:springfox-boot-starter:3.0.0")
+	implementation("io.springfox:springfox-swagger-ui:3.0.0")
+	implementation("io.springfox:springfox-swagger2:3.0.0")
+//	implementation("javax.servlet:javax.servlet-api:4.0.1")
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
--- weather/src/main/java/com/example/weather/controller/DiaryController.java
+++ weather/src/main/java/com/example/weather/controller/DiaryController.java
@@ -2,6 +2,8 @@ package com.example.weather.controller;
 
 import com.example.weather.domain.Diary;
 import com.example.weather.service.DiaryService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,29 +19,42 @@ public class DiaryController {
         this.diaryService = diaryService;
     }
 
+    @ApiOperation(value = "일기 텍스트와 날씨를 이용해서 DB에 일기저장", notes = "일기를 생성하는 api입니다")
     @PostMapping("/create/diary")
-    void createDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date, @RequestBody String text) throws IOException {
+    void createDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+                     @ApiParam(value = "날짜 형식: yyyy-mm-dd", example = "2024-01-01") LocalDate date,
+                     @RequestBody String text) throws IOException {
         diaryService.createDiary(date, text);
     }
 
+    @ApiOperation("선택한 날짜의 모든 일기 데이터를 가져옵니다")
     @GetMapping("/read/diary")
-    List<Diary> readDiary(@RequestParam  @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date){
+    List<Diary> readDiary(@RequestParam  @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+                          @ApiParam(value = "날짜 형식: yyyy-mm-dd", example = "2024-01-01") LocalDate date){
         return diaryService.readDiary(date);
     }
 
+    @ApiOperation("선택한 기간중의 모든 일기 데이터를 가져옵니다")
     @GetMapping("/read/diaries")
-    List<Diary> readDiaries(@RequestParam @DateTimeFormat (iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
-                            @RequestParam @DateTimeFormat (iso = DateTimeFormat.ISO.DATE) LocalDate endDate){
+    List<Diary> readDiaries(@RequestParam @DateTimeFormat (iso = DateTimeFormat.ISO.DATE)
+                            @ApiParam(value = "조회할 기간의 첫번째 날", example = "2024-01-01") LocalDate startDate,
+                            @RequestParam @DateTimeFormat (iso = DateTimeFormat.ISO.DATE)
+                            @ApiParam(value = "조회할 기간의 마지막 날", example = "2024-01-01") LocalDate endDate){
         return diaryService.readDiaries(startDate, endDate);
     }
 
+    @ApiOperation("선택한 날짜의 일기 데이터를 수정합니다")
     @PutMapping("/update/diary")
-    void updateDiary(@RequestParam @DateTimeFormat (iso = DateTimeFormat.ISO.DATE) LocalDate date, @RequestBody String text){
+    void updateDiary(@RequestParam @DateTimeFormat (iso = DateTimeFormat.ISO.DATE)
+                     @ApiParam(value = "날짜 형식: yyyy-mm-dd", example = "2024-01-01")LocalDate date,
+                     @RequestBody String text){
         diaryService.updateDiary(date, text);
     }
 
+    @ApiOperation("선택한 날짜의 일기 데이터를 삭제합니다")
     @DeleteMapping("/delete/diary")
-    void deleteDiary(@RequestParam @DateTimeFormat (iso = DateTimeFormat.ISO.DATE) LocalDate date){
+    void deleteDiary(@RequestParam @DateTimeFormat (iso = DateTimeFormat.ISO.DATE)
+                     @ApiParam(value = "날짜 형식: yyyy-mm-dd", example = "2024-01-01") LocalDate date){
         diaryService.deleteDiary(date);
     }
 
